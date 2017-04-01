43ca84469798246ee6108f6e00466a6bde15edb0
Lucien <Lucien@Lucien-PC.lan>
2017-04-01 13:22:48 +0200

    test

--- src/pProgra/PPMain.java
+++ src/pProgra/PPMain.java
@@ -30,7 +30,9 @@ public class PPMain {
 		int c = JobS.amountSameNumb(testt2.getJobS(), 0, 0);
 		System.out.println(c);
 		System.out.println(JobS.numberCounter(tab2[1], 2));
-		System.out.println("change is good");
+		System.out.println("change is good too");
+		System.out.println("change is good too");
+		System.out.println("change is good too");
 		}
 		
 		
